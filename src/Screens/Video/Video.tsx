bc57e8d3cada436eff5bced3acf2b9ab01aa4504
Aditya kumar <adityakmr9672@gmail.com>
2020-09-12 10:37:16 +0530

    video played

--- src/Screens/Video/Video.tsx
+++ src/Screens/Video/Video.tsx
@@ -1,14 +1,40 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { StackNavProps } from "..";
+import { Video as VideoPlayer } from "expo-av";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 const Video = ({ navigation, route }: StackNavProps<"video">) => {
   const { item } = route.params;
 
+  useEffect(() => {
+    changeScreenOrientation();
+    return () => {
+      changeScreenToPotrait();
+    };
+  }, []);
+
+  async function changeScreenOrientation() {
+    await ScreenOrientation.lockAsync(
+      ScreenOrientation.OrientationLock.LANDSCAPE
+    );
+  }
+  async function changeScreenToPotrait() {
+    await ScreenOrientation.lockAsync(
+      ScreenOrientation.OrientationLock.PORTRAIT
+    );
+  }
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{item.filename}</Text>
-    </View>
+    <VideoPlayer
+      source={{ uri: item.uri }}
+      rate={1.0}
+      volume={1.0}
+      isMuted={false}
+      resizeMode="cover"
+      shouldPlay
+      isLooping
+      style={{ ...StyleSheet.absoluteFillObject }}
+    />
   );
 };
 
